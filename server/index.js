8834f577314735fb626f0476ec26111d45142b91
Sarvesh Choudhary <72163818+sarveshguru@users.noreply.github.com>
2025-08-16 21:59:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -40,6 +40,6 @@ app.get("/", (req, res) => {
 setupSocket(server);
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
